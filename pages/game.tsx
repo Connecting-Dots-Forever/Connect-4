8a7f2efd5afe196567985690884267b79e6b3c1b
idris110 <ratlamwalaidris110@gmail.com>
2023-05-25 11:22:26 +0530

    On click event

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,11 +1,11 @@
 import { log } from 'console';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import styles from "styles/Game.module.css";
 
 function Game() {
 
-  const [turn, setTurn] = useState(1);
+  const [turn, setTurn] = useState(1); //1-for player1, 2 for player 2
   const [ds, setDs] = useState([
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
@@ -14,61 +14,64 @@ function Game() {
     [0, 0, 0, 0, 0, 2, 0],
     [0, 0, 0, 1, 0, 1, 2]
   ]);
+
   const [hoverTop, setHoverTop] = useState(
-    [1, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0]
   )
 
   const bgOnTop = (e) => {
     // e.target.style.background = 'red';
-    console.log(e.target.id.charAt(2), "on");
-    console.log(hoverTop);
-    
+    // console.log(e.target.id.charAt(2), "on");
+    // console.log(hoverTop);
+
     const col = e.target.id.charAt(2);
-    let newTop = hoverTop;
+    let newTop = [0,0,0,0,0,0,0];
     newTop[parseInt(col)] = 1;
-    setHoverTop(newTop)
-    
+    setHoverTop(newTop);
+
   }
 
-  const bgOffTop = (e) => {
-    // e.target.style.background = 'white';
-    // console.log(e.target.id.charAt(0), "off");
-    const col = e.target.id.charAt(2);
-    let newTop = hoverTop;
-    newTop[parseInt(col)] = 0;
-    setHoverTop(newTop)
+  const placeDisk = (e) => {
+    console.log(e.target.id.charAt(2), "place");
+    setTurn((prevTurn)=>((prevTurn === 1) ? 2:1))
   }
 
   return (<>
     <div className='container m-4 lg:mx-auto md:mx-auto xl:mx-auto'>Game</div>
-    <div className="mt-5">
+    <div className="flex justify-center items-end">
 
-      <div className={`${styles.grTop}`}>
-        {hoverTop.map((col, i)=>(
-          <div className={styles.cellTop}
-          key={i}
-          style={{ "backgroundColor": (hoverTop[i]) ? ((turn === 1) ? "red" : "yellow") : "white" }}
-          ></div>
-        ))}
-      </div>
-      
-      <div className={styles.gr}>
+      <div className="mt-5">
 
-        {ds.map((row, x) => {
-          return(
+        <div className={`${styles.grTop} mb-4`}>
+          {hoverTop.map((col, i) => (
+            <div className={styles.cellTop}
+              key={i}
+              style={{ "backgroundColor": (hoverTop[i]) ? ((turn === 1) ? "red" : "yellow") : "white" }}
+              onMouseOver={bgOnTop}
+              onClick={placeDisk}
+              id={`t-${i}`}
+            ></div>
+          ))}
+        </div>
 
-            row.map((cell, y) => {
-              return (<div className={styles.cell}
-                style={{ "backgroundColor": (ds[x][y]) ? ((ds[x][y] === 1) ? "red" : "yellow") : "white" }}
-                onMouseOver={bgOnTop}
-                onMouseLeave={bgOffTop}
-                id={`${x}-${y}`}
-                key={`${x}-${y}`}
-              ></div>)
-            })
-          )
-        })}
-      
+        <div className={`${styles.gr} bg-black p-2`}>
+
+          {ds.map((row, x) => {
+            return (
+
+              row.map((cell, y) => {
+                return (<div className={styles.cell}
+                  style={{ "backgroundColor": (ds[x][y]) ? ((ds[x][y] === 1) ? "red" : "yellow") : "white" }}
+                  onMouseOver={bgOnTop}
+                  onClick={placeDisk}
+                  id={`${x}-${y}`}
+                  key={`${x}-${y}`}
+                ></div>)
+              })
+            )
+          })}
+
+        </div>
       </div>
     </div>
   </>
